72d17395d5e59af771e8e966f6e811649e53f70a
theakhliddin <ak1287@rit.edu>
2024-09-05 09:33:53 +0400

    versionm1

---
+++ happy_birthday.py
@@ -0,0 +1,5 @@
+name = input("What's your name?: ")
+month = input("Which month you were born?: ")
+day = input("Which date you were born?: ")
+year = input("Which year you were born?: ")
+print("Your name is: ", name,'the month you were born: ', month,"birth date: ", day,'year you were born: ', year)
