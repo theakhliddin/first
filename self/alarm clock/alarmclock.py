ea7b6d27cebd3d3093d9c56fda5cb001a098f8af
@theakhliddin <ak1287@rit.edu>
2025-01-21 10:52:50 +0400

    commit

--- self/alarm clock/alarmclock.py
+++ self/alarm clock/alarmclock.py
@@ -21,3 +21,19 @@ def actual_time():
 
 clock = Tk()
 
+clock.title("Alarm Clock")
+clock.geometry("400x200")
+time_format=Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=120)
+addTime = Label(clock,text = "Hour Min Sec",font=60).place(x = 110)
+setYourAlarm = Label(clock,text = "When to wake you up",fg="blue",relief = "solid",font=("Helevetica",7,"bold")).place(x=0, y=29)
+
+hour = StringVar()
+min = StringVar()
+sec = StringVar()
+
+hourTime= Entry(clock,textvariable = hour,bg = "pink",width = 15).place(x=110,y=30)
+minTime= Entry(clock,textvariable = min,bg = "pink",width = 15).place(x=150,y=30)
+secTime = Entry(clock,textvariable = sec,bg = "pink",width = 15).place(x=200,y=30)
+
+submit = Button(clock,text = "Set Alarm",fg="red",width = 10,command = actual_time).place(x =110,y=70)
+clock.mainloop()
